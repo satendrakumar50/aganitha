a405e7bda2ea0a43c4aa073b8e74588404db5c28
satendrakumar50 <gk1293123@gmail.com>
2025-11-23 22:41:13 +0530

    frontend ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,9 @@ return (
 <div className="min-h-screen bg-gray-50 p-6">
 <div className="max-w-3xl mx-auto">
 <h1 className="text-3xl font-bold mb-4">TinyURL — minimal</h1>
-<ShortenForm onCreated={fetchLinks} apiBase="http://localhost:4000" />
+{/* <ShortenForm onCreated={fetchLinks} apiBase="http://localhost:4000" /> */}
+
+<ShortenForm onCreated={fetchLinks} apiBase={API} />
 <LinksList links={links} apiBase={API} />
 </div>
 </div>
